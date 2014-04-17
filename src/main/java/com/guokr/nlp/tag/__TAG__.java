8ddb77adb8b24f9ee0f27d6bef89acc9e5df5151
Mingli Yuan <mingli.yuan@gmail.com>
2014-04-17 17:29:36 +0800

    totally rewrite: remove reflection and customized classloading

--- src/main/java/com/guokr/nlp/tag/__TAG__.java
+++ src/main/java/com/guokr/nlp/tag/__TAG__.java
@@ -1,27 +1,22 @@
 package com.guokr.nlp.tag;
 
-import java.lang.reflect.Method;
+import com.guokr.nlp.seg.__SEG__;
 
 public enum __TAG__ {
 
-    INSTANCE;
+	INSTANCE;
 
-    private Class  clz    = __PKG__.INSTANCE.load("com.guokr.nlp.tag.TagWrapper");
-    private Object tagger = __PKG__.INSTANCE.create("com.guokr.nlp.tag.TagWrapper");
+	private TagWrapper tagger = new TagWrapper();
 
-    public String tag(String text) {
-        String result = null;
-        try {
-            text = com.guokr.nlp.seg.__SEG__.INSTANCE.segment(text);
-            Class[] argTypes = new Class[] { String.class };
-            String[] args = new String[]{ text };
-            Method mtd = this.clz.getDeclaredMethod("tag", argTypes);
-            result = mtd.invoke(this.tagger, args).toString();
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
-        }
-        return result;
-    }
+	public String tag(String text) {
+		String result = null;
+		try {
+			text = __SEG__.INSTANCE.segment(text);
+			result = tagger.tag(text);
+		} catch (Exception e) {
+			e.printStackTrace(System.err);
+		}
+		return result;
+	}
 
 }
-
--- src/test/java/com/guokr/nlp/tag/test/BasicTests.java
+++ src/test/java/com/guokr/nlp/tag/test/BasicTests.java
@@ -1,7 +1,5 @@
 package com.guokr.nlp.tag.test;
 
-import java.lang.reflect.Method;
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -13,10 +11,10 @@ import com.guokr.nlp.tag.__TAG__;
 @RunWith(JUnit4.class)
 public class BasicTests {
 
-    @Test
-    public void testTag() throws Exception {
-        String tagText = __TAG__.INSTANCE.tag("这是个测试");
-        assertEquals("这#PN 是#VC 个#M 测试#NN", tagText);
-    }
+	@Test
+	public void testTag() throws Exception {
+		String tagText = __TAG__.INSTANCE.tag("这是个测试");
+		assertEquals("这#PN 是#VC 个#M 测试#NN", tagText);
+	}
 
 }
